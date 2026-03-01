adf8366156689496c706ff814296b671307ec01e
wise <wisebuh3@gmail.com>
2026-03-01 05:59:00 +0100

    structered the app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-black"
+    >
+      <Text className="text-white font-bold text-3xlr">Edit app/index.tsx to edit this screen 22223.</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import "./global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack 
+  screenOptions={{
+    headerShown:false,
+  }}>
+    <Stack.Screen
+    name="(tabs)"
+    />
+  </Stack>;
 }
